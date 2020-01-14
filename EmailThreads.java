99863bfa5f05da6cb34856109bec82168752f534
vpsoma <56629386+vpsoma@users.noreply.github.com>
2020-01-14 11:12:38 +0200

    Update EmailThreads.java

--- EmailThreads.java
+++ EmailThreads.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
  * @author vpsoma
  */
 
+
 public class EmailThreads {
 	/**
 	 * This method gets a number of mails that need to get sent
@@ -18,20 +19,7 @@ public class EmailThreads {
 	 * @returns Nothing
 	 * @throws Exception
 	 */
-	
-	public static void generateThreads(int numOfClients, String[] Clients, String[] Names, String[] Products) throws Exception {
-		/**
-		 * This method gets a number of mails that need to get sent
-		 * and splits the arrays it gets according to this number.
-		 * Then, it creates new JavaMail threads and starts them.
-		 * @param numOfClients an integer 
-		 * 		  Clients 	   a String array of email addresses
-		 * 		  Names  	   a String array of names
-		 * 		  Products	   a String array of email addresses
-		 * @returns Nothing
-		 * @throws Exception
-		 */
-		
+	public void generateThreads(int numOfClients, String[] Clients, String[] Names, String[] Products) throws Exception {
 		int num;
 		if (numOfClients %2 == 0) {
 			num = numOfClients / 2;
@@ -49,8 +37,6 @@ public class EmailThreads {
 				partOfNames = Arrays.copyOfRange(Names,i , i + 1);
 				partOfProducts = Arrays.copyOfRange(Products, i, i + 1);
 				JavaMail t = new JavaMail(partOfClients, partOfNames, partOfProducts);
-				InfoMail object = new InfoMail(partOfClients, partOfNames, partOfProducts);
-				JavaMail t = new JavaMail(object);
 				t.start();
 			} catch (Exception e) {
 				System.out.print("An exception has occured. We are sorry.");
