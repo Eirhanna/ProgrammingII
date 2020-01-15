2417a89873697b2c2a601f510a561aa277acafe2
EirhannaPapoutsi <irianna_pap@hotmail.gr>
2020-01-15 04:01:09 +0200

    Uploading the main method

---
+++ Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+
+	public static void main(String[] agrs) {
+		new OptionMenu();
+		
+	}
+	
+	public static void loadObjects() {
+		
+		ProductFactory.purchaseOfStock("Doll and  Bear Toybox", "t8180", 30, 1, 1, 2020, 25);
+		ProductFactory.purchaseOfStock("Colourfull Ball", "t8181", 5, 1, 1, 2020, 10);
+		ProductFactory.purchaseOfStock("HotWheels Car", "t8182", 10, 1, 1, 2020, 15);
+		ProductFactory.purchaseOfStock("Stuffed Duck Abie", "t8183", 20, 1, 1, 2020, 6);
+		ProductFactory.purchaseOfStock("Train Pollar Express", "t8184", 5, 1, 1, 2020, 20);
+		ProductFactory.purchaseOfStock("Stuffed Zebra Martin", "t8185", 28, 1, 1, 2020, 12);
+		ProductFactory.purchaseOfStock("Teddy Bear Molly", "t8186", 13, 1, 1, 2020, 4);
+		ProductFactory.purchaseOfStock("Stuffed Duck Abie", "t8183", 15, 7, 1, 2020, 6);
+		ProductFactory.purchaseOfStock("Colourfull Ball", "t8181", 15, 1, 1, 2020, 10);
+		ProductFactory.purchaseOfStock( "Grand Surprise ToyBox", "t8187", 10, 5, 1, 2020, 25);
+		ProductFactory.purchaseOfStock( "Grand Surprise ToyBox", "t8187", 5, 10, 1, 2020, 25);
+	}
+}
+
+
